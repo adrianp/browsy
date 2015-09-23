0cd971ed7a32274d6e35e858dc6422b0101d4eaf
Adrian-Tudor Panescu <adrian@panescu.com>
2015-09-23 21:33:43 +0300

    browsy.py: address bar

--- browsy.py
+++ browsy.py
@@ -4,7 +4,7 @@ import getopt
 import sys
 
 from PyQt4.QtCore import QUrl
-from PyQt4.QtGui import QApplication
+from PyQt4.QtGui import QApplication, QGridLayout, QLineEdit, QWidget
 from PyQt4.QtWebKit import QWebView
 
 
@@ -13,9 +13,23 @@ class Usage(Exception):
         self.msg = msg
 
 
-def process(arg):
-    # TODO: implement
-    print("arg:", arg)
+class UrlInput(QLineEdit):
+    def __init__(self, browser):
+        super(UrlInput, self).__init__()
+        self.browser = browser
+        # add event listener on "enter" pressed
+        self.returnPressed.connect(self._return_pressed)
+
+    def _return_pressed(self):
+        url = QUrl(self.text())
+        # load url into browser frame
+        self.browser.load(cleanUrl(url))
+        self.browser.show()
+
+
+def cleanUrl(url):
+    # TODO: make sure url is actually navigable (e.g., has http://)
+    return QUrl(url)
 
 
 def main(argv=None):
@@ -31,19 +45,30 @@ def main(argv=None):
             opts, args = getopt.getopt(argv[1:], "h", ["help"])
         except getopt.error, msg:
             raise Usage(msg)
-        for arg in args:
-            process(arg)
-        # TODO: implement option handling
     except Usage, err:
         print >>sys.stderr, err.msg
         print >>sys.stderr, "for help use --help"
         return 2
 
-    app = QApplication(argv)
+    app = QApplication(sys.argv)
+
+    # create grid layout
+    grid = QGridLayout()
 
     browser = QWebView()
-    browser.load(QUrl(argv[1]))
-    browser.show()
+    if len(argv) > 1:
+        browser.load(cleanUrl(argv[1]))
+    # browser frame at row 2 column 0 of our grid
+    grid.addWidget(browser, 2, 0)
+
+    url_input = UrlInput(browser)
+    # url_input at row 1 column 0 of our grid
+    grid.addWidget(url_input, 1, 0)
+
+    # main app window
+    main_frame = QWidget()
+    main_frame.setLayout(grid)
+    main_frame.show()
 
     return app.exec_()
 
