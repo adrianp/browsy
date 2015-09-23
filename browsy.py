8410bd98a6d1b71131b217e65a76c48f005687f4
Adrian-Tudor Panescu <adrian@panescu.com>
2015-09-23 21:11:21 +0300

    browsy.py: better main function

--- browsy.py
+++ browsy.py
@@ -1,13 +1,51 @@
+"""Browsy - A Qt Web Browser written in Python.
+"""
+import getopt
 import sys
 
-from PyQt4.QtWebKit import QWebView
-from PyQt4.QtGui import QApplication
 from PyQt4.QtCore import QUrl
+from PyQt4.QtGui import QApplication
+from PyQt4.QtWebKit import QWebView
 
-app = QApplication(sys.argv)
 
-browser = QWebView()
-browser.load(QUrl(sys.argv[1]))
-browser.show()
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
 
-app.exec_()
+
+def process(arg):
+    # TODO: implement
+    print("arg:", arg)
+
+
+def main(argv=None):
+    if argv is None:
+        # you don't want to use a sys.argv as a defaul argument as it would be
+        # set when the function is defined, *not* when it's called
+        argv = sys.argv
+
+    try:
+        # parse command line options
+        try:
+            # process options and argument
+            opts, args = getopt.getopt(argv[1:], "h", ["help"])
+        except getopt.error, msg:
+            raise Usage(msg)
+        for arg in args:
+            process(arg)
+        # TODO: implement option handling
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "for help use --help"
+        return 2
+
+    app = QApplication(argv)
+
+    browser = QWebView()
+    browser.load(QUrl(argv[1]))
+    browser.show()
+
+    return app.exec_()
+
+if __name__ == "__main__":
+    sys.exit(main())
